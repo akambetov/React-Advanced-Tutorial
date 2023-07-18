691c13d9ba21ee188d0d4d75c65e5fc58467e227
akambetov <almat_kambetov@mail.ru>
2023-07-18 22:17:54 +0600

    lsn-6/4: Counter component

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 import {render } from 'react-dom'
+import { Counter } from './components/Counter'
 
-render(<div>test</div>, document.getElementById('root'))
+render(<Counter />, document.getElementById('root'))
